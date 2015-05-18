402a0c8e84c4471c8d1534f1d2c1c549d78a7fc0
Henry Lim <henry71896@gmail.com>
2015-05-18 11:55:35 +0800

    Update

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(function () {
+  "use strict";
+
+
+  // Make sure we are accessing over https, if not redirect
+  if ((!location.port || location.port === "80") && location.protocol !== "https:" && location.host !== "localhost") {
+    location.protocol = "https:";
+  }
+
+
+  // Register our ServiceWorker
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register("/worker.js", {
+      scope: "/"
+    }).then(function (reg) {
+      console.log("SW register success", reg);
+    }, function (err) {
+      console.log("SW register fail", err);
+    });
+  }
+
+  function updateNetworkStatus() {
+
+    if (navigator.onLine) {
+      alert("online");
+    } else {
+      alert("offffffline");
+    }
+  }
+  window.addEventListener("online", updateNetworkStatus);
+  window.addEventListener("offline", updateNetworkStatus);
+  updateHistory();
+  updateNetworkStatus();
+}());
